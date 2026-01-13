2f3fa6dfe7d36e71d7f972b930be3ed45d05d47b
Emanuele Contini <emanuelecontini26@gmail.com>
2026-01-13 14:12:00 +0100

    feat: add dto checks for ratings

--- lab-lib-restapi/src/main/java/com/lab_lib/restapi/DTO/Rating/AddRatingToBookRequest.java
+++ lab-lib-restapi/src/main/java/com/lab_lib/restapi/DTO/Rating/AddRatingToBookRequest.java
@@ -1,11 +1,19 @@
 package com.lab_lib.restapi.DTO.Rating;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 
 public class AddRatingToBookRequest {
+    @NotNull
     String ratingName;
+    @NotNull
     String review;
+    @NotNull
+    @Min(1)
+    @Max(5)
     Integer evaluation;
 }
